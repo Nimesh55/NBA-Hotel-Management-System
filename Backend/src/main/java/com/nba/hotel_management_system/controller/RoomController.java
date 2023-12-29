8a49ced3cb22a22ee2d8ee0d162e6cdbf2d689a4
Nimesh Ariyarathne <80301441+Nimesh55@users.noreply.github.com>
2023-12-29 14:36:14 +0530

    Completed add room functionality
    Completed all frontend and backend parts

--- Backend/src/main/java/com/nba/hotel_management_system/controller/RoomController.java
+++ Backend/src/main/java/com/nba/hotel_management_system/controller/RoomController.java
@@ -4,32 +4,40 @@ import com.nba.hotel_management_system.model.Room;
 import com.nba.hotel_management_system.response.RoomResponse;
 import com.nba.hotel_management_system.service.IRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.List;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/rooms")
 public class RoomController {
 
+    @Autowired
     private IRoomService roomService;
 
     @PostMapping("/add/new-room")
     public ResponseEntity<RoomResponse> addNewRoom(
            @RequestParam("photo") MultipartFile photo,
            @RequestParam("roomType")String roomType,
-           @RequestParam("roomPrice")BigDecimal roomPrice) {
+           @RequestParam("roomPrice")BigDecimal roomPrice) throws SQLException, IOException {
 
         Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
 
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(),
                 savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomTypes();
     }
 }
--- Backend/src/main/java/com/nba/hotel_management_system/repository/RoomRepository.java
+++ Backend/src/main/java/com/nba/hotel_management_system/repository/RoomRepository.java
@@ -2,6 +2,12 @@ package com.nba.hotel_management_system.repository;
 
 import com.nba.hotel_management_system.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
