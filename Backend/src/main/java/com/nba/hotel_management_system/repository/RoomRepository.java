ceca6054f8b0a6ec28185b21df993c6248973f89
Nimesh Ariyarathne <80301441+Nimesh55@users.noreply.github.com>
2023-12-31 13:06:42 +0530

    Availabel room search frontend added

--- Backend/src/main/java/com/nba/hotel_management_system/repository/RoomRepository.java
+++ Backend/src/main/java/com/nba/hotel_management_system/repository/RoomRepository.java
@@ -12,8 +12,11 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("SELECT DISTINCT r.roomType FROM Room r")
     List<String> findDistinctRoomTypes();
 
-    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id" +
-            " NOT IN (SELECT br.room.id FROM BookedRoom br WHERE ((br.checkInDate<= :checkOutData)" +
-            " AND (br.checkOutDate>+ :checkInDate)))")
+    @Query(" SELECT r FROM Room r " +
+            " WHERE r.roomType LIKE %:roomType% " +
+            " AND r.id NOT IN (" +
+            "  SELECT br.room.id FROM BookedRoom br " +
+            "  WHERE ((br.checkInDate <= :checkOutDate) AND (br.checkOutDate >= :checkInDate))" +
+            ")")
     List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
